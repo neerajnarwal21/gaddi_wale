b80287b1b084d8c052f9b970ecfb0e2c43c14fe2
Neeraj Narwal <neeraj.n@pugmarks.com>
2019-06-27 13:10:40 +0530

    Complete code

---
+++ app/src/main/java/com/gaddi/wale/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.gaddi.wale
+
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.webkit.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView.loadUrl("http://www.gaddiwale.in")
+        webView.settings.cacheMode = WebSettings.LOAD_DEFAULT
+        webView.settings.javaScriptEnabled = true
+        webView.settings.builtInZoomControls = true
+        webView.settings.displayZoomControls = false
+        webView.webViewClient = object : WebViewClient() {
+
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                if (logoIV.visibility == View.GONE) {
+                    progressPB.visibility = View.VISIBLE
+                }
+                return super.shouldOverrideUrlLoading(view, request)
+
+            }
+
+            override fun onPageCommitVisible(view: WebView?, url: String?) {
+                super.onPageCommitVisible(view, url)
+                logoIV.visibility = View.GONE
+                progressPB.visibility = View.GONE
+                swipeRL.isRefreshing = false
+            }
+        }
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                super.onProgressChanged(view, newProgress)
+                progressPB.progress = newProgress
+            }
+        }
+        swipeRL.setOnRefreshListener { webView.reload() }
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else
+            showExit()
+    }
+
+    fun showExit() {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("Are you sure you want to exit ?")
+            .setCancelable(false)
+            .setPositiveButton("Yes") { _, _ -> finishAffinity() }
+            .setNegativeButton("No", null)
+        val alert = builder.create()
+        alert.show()
+    }
+}
